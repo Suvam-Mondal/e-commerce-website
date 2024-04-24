a036d64975adc429b04f3ab9a077b36518c5ab9a
Suvam Mondal <s.mondal201094@gmail.com>
2024-04-24 15:24:27 +0530

    code clean up

--- e-commerce-frontend/src/app/utils/sort-products.pipe.ts
+++ e-commerce-frontend/src/app/utils/sort-products.pipe.ts
@@ -10,8 +10,6 @@ export class SortProductsPipe implements PipeTransform {
   transform(value: ProductModel[], arg: string): ProductModel[] {
     if (arg === 'price') {
       return value.slice().sort((a, b) => a.price - b.price);
-    } else if (arg === 'rating') {
-      return value; /*value.slice().sort((a, b) => b.rating - a.rating);*/
     } else {
       return value;
     }
